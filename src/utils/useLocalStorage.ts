3f097f95b3d4fdd421a5d3bfd33d4eb348587c5a
lsimonar <lluis.simon.92@gmail.com>
2022-12-12 23:07:10 +0100

    changed default value to unknown

--- src/utils/useLocalStorage.ts
+++ src/utils/useLocalStorage.ts
@@ -1,13 +1,13 @@
 import { useState, useEffect } from "react";
 
-function getStorageValue(key: string, defaultValue: any) {
+function getStorageValue(key: string, defaultValue: unknown) {
   // getting stored value
   const saved = localStorage.getItem(key);
   const initial = JSON.parse(saved!);
   return initial || defaultValue;
 }
 
-export const useLocalStorage = (key: string, defaultValue: any) => {
+export const useLocalStorage = (key: string, defaultValue: unknown) => {
   const [value, setValue] = useState(() => {
     return getStorageValue(key, defaultValue);
   });
